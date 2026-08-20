fc28d4c18c0bad0d388738495768dc7cec2ce98f
CosminB24 <cosminbadea919@yahoo.com>
2026-08-20 17:02:02 +0300

    CLI parses ecosystem and target

--- cmd/detonate/main.go
+++ cmd/detonate/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("detonate: ready")
+	const requiredArgs = 3
+
+	if len(os.Args) < requiredArgs {
+		fmt.Fprintln(os.Stderr, "Usage: detonate npm <package>")
+		os.Exit(1)
+	}
+
+	if os.Args[1] != "npm" {
+		fmt.Fprintln(os.Stderr, "Only npm is supported")
+		os.Exit(1)
+	}
+
+	fmt.Printf("%-10s %s\n", "ecosystem:", os.Args[1])
+	fmt.Printf("%-10s %s\n", "target:", os.Args[2])
 }
